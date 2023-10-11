e793de22611acdbb3a7c8df2892e2232b2917e9f
Martin Njau <njau_martin@yahoo.comgit config --global user.email njau_martin@yahoo.comclearclear>
2023-10-11 12:35:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+let arrMsgs=[];
+function writeCards(names , surprise) {
+    //let arrMsgs=[];
+for (let i=0; i<names.length; i++) {
+    console.log(arrMsgs.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`));
+}
+return arrMsgs;
+}
+
+function countDown(num) {
+  let age=num;
+    while(age>=0){
+        console.log(age--);
+    }
+}
+countDown(10);
+    
